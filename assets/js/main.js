a90563b1c18c50a52f58c4fe4a0fb26969ce8672
Sharmyn28 <sharmynrodriguez@gmail.com>
2017-09-24 10:19:47 -0500

    add task working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,190 +1,210 @@
+'use strict';
 
-function Task (title, duration){
-	this.title = title;
-	this.duration = duration;
-	this.isCompleted = false;
+class Task{
+	constructor(title, duration){
+		this.title = title;
+		this.duration = duration;
+		this.isCompleted = false;
+	}
 }
 
-function List(){
-	this.tasks =[];
-	this.initialTasks =[
-	  {
-	    "userId": 1,
-	    "id": 1,
-	    "title": "Delectus aut autem",
-	    "completed": false
-	  },
-	  {
-	    "userId": 1,
-	    "id": 2,
-	    "title": "Quis ut nam facilis et officia qui",
-	    "completed": false
-	  },
-	  {
-	    "userId": 1,
-	    "id": 3,
-	    "title": "Fugiat veniam minus",
-	    "completed": false
-	  },
-	  {
-	    "userId": 1,
-	    "id": 4,
-	    "title": "Et porro tempora",
-	    "completed": true
-	  },
-	  {
-	    "userId": 1,
-	    "id": 5,
-	    "title": "Laboriosam mollitia et enim quasi adipisci quia provident illum",
-	    "completed": false
-	  },
-	  {
-	    "userId": 1,
-	    "id": 6,
-	    "title": "Qui ullam ratione quibusdam voluptatem quia omnis",
-	    "completed": false
-	  },
-	  {
-	    "userId": 1,
-	    "id": 7,
-	    "title": "Illo expedita consequatur quia in",
-	    "completed": false
-	  },
-	  {
-	    "userId": 1,
-	    "id": 8,
-	    "title": "Quo adipisci enim quam ut ab",
-	    "completed": true
-	  },
-	  {
-	    "userId": 1,
-	    "id": 9,
-	    "title": "Molestiae perspiciatis ipsa",
-	    "completed": false
-	  },
-	  {
-	    "userId": 1,
-	    "id": 10,
-	    "title": "Illo est ratione doloremque quia maiores aut",
-	    "completed": true
-	  },
-	  {
-	    "userId": 1,
-	    "id": 11,
-	    "title": "Vero rerum temporibus dolor",
-	    "completed": true
-	  },
-	]
-
-	this.add = function(task){
+class List{
+	constructor(){
+		this.tasks =[];
+		this.initialTasks =[
+			{
+				"userId": 1,
+				"id": 1,
+				"title": "Delectus aut autem",
+				"completed": false
+			},
+			{
+				"userId": 1,
+				"id": 2,
+				"title": "Quis ut nam facilis et officia qui",
+				"completed": false
+			},
+			{
+				"userId": 1,
+				"id": 3,
+				"title": "Fugiat veniam minus",
+				"completed": false
+			},
+			{
+				"userId": 1,
+				"id": 4,
+				"title": "Et porro tempora",
+				"completed": true
+			},
+			{
+				"userId": 1,
+				"id": 5,
+				"title": "Laboriosam mollitia et enim quasi adipisci quia provident illum",
+				"completed": false
+			},
+			{
+				"userId": 1,
+				"id": 6,
+				"title": "Qui ullam ratione quibusdam voluptatem quia omnis",
+				"completed": false
+			},
+			{
+				"userId": 1,
+				"id": 7,
+				"title": "Illo expedita consequatur quia in",
+				"completed": false
+			},
+			{
+				"userId": 1,
+				"id": 8,
+				"title": "Quo adipisci enim quam ut ab",
+				"completed": true
+			},
+			{
+				"userId": 1,
+				"id": 9,
+				"title": "Molestiae perspiciatis ipsa",
+				"completed": false
+			},
+			{
+				"userId": 1,
+				"id": 10,
+				"title": "Illo est ratione doloremque quia maiores aut",
+				"completed": true
+			},
+			{
+				"userId": 1,
+				"id": 11,
+				"title": "Vero rerum temporibus dolor",
+				"completed": true
+			}
+		]
+	}
+	add(task){
 		this.tasks.push(task);
 	}
-
-	this.toHTML = function (arr, numL){
-		var lista = document.getElementById("lista");
-		var listaNueva = document.getElementById("lista nueva");
-
+	
+	toHTML(arr, numL){
 		if(numL == 1){
-			for(var i in arr){
-		   		lista.innerHTML += '<form action="#"><p><input type="checkbox" name="lis" id="'+i+'"> <label for="'+i+'" class = "task">'+arr[i].title+'<a class="remove-task">remove</a></label></p></form>';
-			}
+			$.each(arr, function(index, value){
+				$("#list").append(`<form action="#">
+										<p><input type="checkbox" name="lis" id="${index}"><label for="${index}" class = "task">${value.title}<a class="remove-task">remove</a></label></p>
+		 							</form>`);
+			});
 			return true;
 		}else if (numL == 2){
-			var str = "";
-			for(var j = 11 ; j < (11 + arr.length); j++){
-				for(var x in arr){ 
-					console.log("/"+arr[x].title);  
-		   			str+= '<form action="#"><p><input type="checkbox" name="lis" id="'+j+'"> <label for="'+j+'" class = "task">'+arr[x].title+'</label></p></form>';
-		   		}
-		   		listaNueva.innerHTML = str;
-		   		return true;
+			for(let j = 11 ; j < (11 + arr.length); j++){ 
+				$.each(arr, function(index, value){
+					console.log("/"+value.title); 
+					$('#new_List').append(`<form action="#">
+												<p><input type="checkbox" name="lis" id="${j}"><label for="${j}" class = "task">${value.title}</label></p>
+				 							</form>`);
+					});
+				return true;
 			}
 		}
 	}
-
-	this.newTask = function (){
 	
-		//var newTT = prompt("Escriba nueva tarea");
-		//var newTD = prompt("Escriba la duracion de la tarea");
+	newTask(){
 		console.log(this.tasks);
 		swal({
-            title: "¿Que tarea desea agregar?",
-            text: "Ingrese una nueva tarea",
-            type: "input",
-            showCancelButton: true,
-            closeOnConfirm: false,
-            animation: "slide-from-top",
-            inputPlaceholder: "Write something"
-        	},
-            function(inputValue){
-              if (inputValue === false) return false;
-
-              if (inputValue === "") {
-                  swal.showInputError("Debes ingresar una tarea");
-                  return false
-              }
-            var newTT = inputValue;
-    
-            swal({
-                  title: "¿Cuánto tiempo durara?",
-                  text: "Ingrese la duracion de la tarea",
-                  type: "input",
-                  showCancelButton: true,
-                  closeOnConfirm: false,
-                  animation: "slide-from-top",
-                  inputPlaceholder: "Write something"
-                },
-                  function(inputValue){
-                    if (inputValue === false) return false;
-
-                    if (inputValue === "") {
-                      swal.showInputError("Debes ingresar un número");
-                      return false
-                    }
-                    var newTD = inputValue;
-                  	newTD = parseInt(newTD);
-                       
-                swal("Perfecto", "Escribiste: " + newTT + ", "+ newTD + " minutos", "success");
-				
-//se instancio Task dentro del metodo puesto que por propiedades del uso de la libreria sweetAlert no capturaba el array
-				var newT = new Task(newTT, newTD);
-				
-				list.tasks.push(newT);
-				list.toHTML(list.tasks,2);
-				console.log(list.tasks);
-				//return newT;
-              });             
-      });
+			title: "What task do you want to add?",
+			text: "Please add new task",
+			type: "input",
+			showCancelButton: true,
+			closeOnConfirm: false,
+			animation: "slide-from-top",
+			inputPlaceholder: "Write something"
+			},
+			(inputValue)=>{
+				if (inputValue === false) return false;
+				if (inputValue === "") {
+					swal.showInputError("You should add a task");
+					return false
+				}
+			let newTT = inputValue;
+			swal({
+				title: "How long this task will take?",
+				text: "Add the duration of the task",
+				type: "input",
+				showCancelButton: true,
+				closeOnConfirm: false,
+				animation: "slide-from-top",
+				inputPlaceholder: "Write something"
+			},
+				(inputValue)=>{
+					if (inputValue === false) return false;
+					if (inputValue === "") {
+						swal.showInputError("You should add a number for duration");
+						return false
+					}
+					let newTD = inputValue;
+					newTD = parseInt(newTD);
+											
+					swal("Great", "You have written: " + newTT + ", "+ newTD + " minutes", "success");
+					
+	//se instancio Task dentro del metodo puesto que por propiedades del uso de la libreria sweetAlert no capturaba el array
+					let newT = new Task(newTT, newTD);	
+					this.tasks.push(newT);
+					this.toHTML(this.tasks,2);
+					console.log(this.tasks);
+					//return newT;
+			});             
+		});
 	}
-
-	this.completeTask = function (task) {
+	
+	completeTask(task) {
 		task.classList.toggle('task-complete');
 		//this.tasks.isCompleted = true;
 		//console.log(newT);
 	}
-
-	this.removeTask = function(task){
+	
+	removeTask(task){
 		task.style.opacity = 0;
 		setTimeout(function() {
 			task.remove();
 		}, 400);
 	}
+
+	eventRemove(e){
+		// Remove task
+		if ( e.target.classList.contains('remove-task') ) {
+			this.removeTask(e.target);
+		// Complete Task
+		} else if ( e.target.classList.contains('task') ) {
+			this.completeTask(e.target);
+		}
+	}
+
+	init(){
+		this.toHTML(this.initialTasks,1);
+		$('#btnAdd').click( () => this.newTask());
+		$('input').click( () => this.eventRemove());
+	}
 }
 
+$(document).ready( () => {
+	let list = new List();
+	list.init();
+})
 
+
+/*document.addEventListener('click', function(e) {
+	// Remove task
+	if ( e.target.classList.contains('remove-task') ) {
+		list.removeTask(e.target);
+
+	// Complete Task
+	} else if ( e.target.classList.contains('task') ) {
+		list.completeTask(e.target);
+
+	}
+}, false);*/
+
+
+/*
 var list = new List();
 printInitial();
-
-var btnAdd= document.getElementById("btnAdd");
-btnAdd.onclick = function (){
-	list.newTask();
-};
-
-function printInitial (){
-	var initial = list.initialTasks;
-	list.toHTML(initial,1);
-}
-
 
 document.addEventListener('click', function(e) {
 	// Remove task
@@ -196,7 +216,7 @@ document.addEventListener('click', function(e) {
 		list.completeTask(e.target);
 
 	}
-}, false);
+}, false);*/
 
 
 //*****************EXTRA*************
